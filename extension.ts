ae44ae83737db6b41ee6e50ffe01e53ca58fcd83
Daryl Zuniga <Daryl.Zuniga@gmail.com>
2020-04-10 15:39:28 -0700

    Add sayThen and say

--- extension.ts
+++ extension.ts
@@ -14,16 +14,24 @@ namespace words {
         return new word.Bubble();
     }
 
-    //% block="say $lines then"
+    //% block="$bubble say $lines then"
     //% bubble.shadow=variables_get
     //% bubble.defl=bubble
-    //% handlerStatement
+    //% handlerStatement=1
     //% group="Words" weight=50 blockGap=8 
-    export function sayThen(bubble: word.Bubble, lines: string[], onEnd: () => {}) {
+    export function sayThen(bubble: word.Bubble, lines: string[], onEnd: () => void) {
         let ls = lines.map(l => word.line(l, word.TextSpeed.Medium))
         bubble.startMessage([
             new word.MessagePage(ls)
         ], onEnd);
 
+    }
+
+    //% block="$bubble say $lines"
+    //% bubble.shadow=variables_get
+    //% bubble.defl=bubble
+    //% group="Words" weight=50 blockGap=8 
+    export function say(bubble: word.Bubble, lines: string[]) {
+        sayThen(bubble, lines, () => {})
     }
 }

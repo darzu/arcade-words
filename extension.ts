026883c35bc32a10e01d05ccb9efb398caac6651
Daryl Zuniga <Daryl.Zuniga@gmail.com>
2020-04-17 16:17:19 -0700

    More functions

--- extension.ts
+++ extension.ts
@@ -34,4 +34,40 @@ namespace words {
     export function say(bubble: word.Bubble, lines: string[]) {
         sayThen(bubble, lines, () => {})
     }
+
+    //% block="attach $bubble to $sprite"
+    //% bubble.shadow=variables_get
+    //% bubble.defl=bubble
+    //% sprite.shadow=variables_get
+    //% sprite.defl=sprite
+    //% group="Words" weight=50 blockGap=8 
+    export function setAnchorSprite(bubble: word.Bubble, sprite: Sprite) {
+        bubble.setAnchorSprite(sprite);
+    }
+
+    //% block="position $bubble at x $x y $y"
+    //% bubble.shadow=variables_get
+    //% bubble.defl=bubble
+    //% group="Words" weight=50 blockGap=8 
+    export function setAnchor(bubble: word.Bubble, x: number, y: number) {
+        bubble.setAnchor(x, y)
+    }
+
+    //% block="center $bubble text $center"
+    //% bubble.shadow=variables_get
+    //% bubble.defl=bubble
+    //% group="Words" weight=50 blockGap=8 
+    export function center(bubble: word.Bubble, center: boolean) {
+        bubble.setCentered(center)
+    }
+
+    //% block="set bubble foreground $fg and background $bg"
+    //% fg.shadow="colorindexpicker"
+    //% bg.shadow="colorindexpicker"
+    //% group="Words" weight=50 blockGap=8 
+    export function setColors(fg: number, bg: number) {
+        word.setColors(fg, bg);
+    }
+
+    
 }
